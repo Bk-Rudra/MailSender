a2620ab123383de37b51f4acc7dbad7b863cbef9
Bk-Rudra <90374040+Bk-Rudra@users.noreply.github.com>
2023-01-13 10:31:20 +0530

    Add files via upload

---
+++ docs/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+app.use(bodyParser.json());
+var nodemailer = require("nodemailer");
+let PORT = process.env.PORT || 3000;
+app.use(cors({ origin: "*" }));
+
+app.post("/sendmail", (req, res) => {
+  let email, msg, firstName, lastName, mobileNumber;
+  email = req.body.email;
+  msg = req.body.Message;
+  firstName = req.body.Name;
+  lastName = req.body.LastName;
+  mobileNumber = req.body.Mobile;
+  let transporter = nodemailer.createTransport({
+    host: "smtp.cdac.in",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "pki",
+      pass: "pki@India@123",
+    },
+    tls: { secureProtocol: "TLSv1_method" },
+  });
+  let mailOptions = {
+    from: "pki@cdac.in",
+    to: "pki@cdac.in",
+    subject: "New enquiry from " + firstName + " " + lastName,
+    text:
+      msg + "\nFor contact, mail me on: " + email + " or call: " + mobileNumber,
+  };
+  transporter.sendMail(mailOptions, (err, success) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Sent successfully!");
+    }
+  });
+  res.sendStatus(200);
+});
+app.get("/hello", (req, res) => {
+  res.send("hello am working");
+});
+app.listen(PORT);
